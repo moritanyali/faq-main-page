9a4b27fa7af9234cb3cf4c43c9ee43f818850797
moritanyali <111087985+moritanyali@users.noreply.github.com>
2022-10-21 01:14:37 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+$('.arrow-1, .p-q-1').click(function(){
+    $('.p-q-1').toggleClass('bold-text');
+    $('.q-t-1').addClass('show-text').slideToggle('medium', function(){
+      
+
+    });
+});
+
+$('.arrow-2, .p-q-2').click(function(){
+    $('.p-q-2').toggleClass('bold-text');
+    $('.q-t-2').addClass('show-text').slideToggle('medium', function(){
+       
+    });
+});
+
+$('.arrow-3, .p-q-3').click(function(){
+    $('.p-q-3').toggleClass('bold-text');
+    $('.q-t-3').addClass('show-text').slideToggle('medium', function(){
+        
+    });
+});
+
+$('.arrow-4, .p-q-4').click(function(){
+    $('.p-q-4').toggleClass('bold-text');
+    $('.q-t-4').addClass('show-text').slideToggle('medium', function(){
+        
+    });
+});
+
+$('.arrow-5, .p-q-5').click(function(){
+    $('.p-q-5').toggleClass('bold-text');
+    $('.q-t-5').addClass('show-text').slideToggle('medium', function(){
+        
+    });
+});
+
